c3371928b1fe7defee194926c2082e5a46b88a78
HJ Gwak <51011169+pix3000@users.noreply.github.com>
2023-05-18 18:00:05 +0900

    Update find_words.py

--- find_words.py
+++ find_words.py
@@ -13,8 +13,8 @@ def get_most_common_words(file_path, num_words=10):
         for page in reader.pages:
             text += page.extract_text()
         
-        # 숫자, 전치사, 문장기호, 3자리 미만 단어 제외한 단어 추출
-        words = re.findall(r'\b(?!(?:in|on|at|for|to|with|a|an|the|[.,!?;:]|\d|\b\w{1,2}\b))\w+\b', text, flags=re.IGNORECASE)
+        # 숫자, 전치사, 문장기호, 5자 이상 단어 추출
+        words = re.findall(r'\b(?!(?:in|on|at|for|to|with|a|an|the|[.,!?;:]|\d|\b\w{1,4}\b))\w+\b', text, flags=re.IGNORECASE)
         
         # 단어 빈도수 계산
         word_counter = Counter(words)
@@ -30,5 +30,6 @@ args = parser.parse_args()
 
 # 가장 많이 나온 단어 출력
 most_common_words = get_most_common_words(args.pdf_file, args.num_words)
+print(f'PDF NAME: {args.pdf_file}\n')
 for word, count in most_common_words:
     print(f'{word}: {count}')
